458e46e44066bf85f3b86769280dabec2722cc78
Lucas <neexdeveloper@gmail.com>
2022-07-07 21:58:27 -0300

    feat(*): admin painel WIP

--- admin/includes/view/_Head.php
+++ admin/includes/view/_Head.php
@@ -6,14 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sigma Admin</title>
 
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/98c1a0f48e.js" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
         integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/global.css">
     <link rel="stylesheet" href="../css/global.css">
 </head>
 <body>
-    <script src="./js/font.js"></script>
-    <script src="../js/font.js"></script>
+    <script src="./js/scripts.js"></script>
+    <script src="../js/scripts.js"></script>
 </body>
 </html>
--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@ require_once "../main/classes/Database.php";
 $databaseClass = new Database();
 
 include_once "./includes/view/_Head.php";
+include_once "./includes/view/_Header.php";
 
 if (!isset($_SESSION["LOGIN"]) || empty($_SESSION["LOGIN"])) {
   header("location: ./pages/login.php");
@@ -13,69 +14,59 @@ if (!isset($_SESSION["LOGIN"]) || empty($_SESSION["LOGIN"])) {
 
 $username = $_SESSION["LOGIN"]["username"];
 
+$metrics = [];
+$countQuery = $databaseClass->query("SELECT 'categories' AS table_name, COUNT(*) AS `amount` FROM categories UNION SELECT 'products' AS table_name, COUNT(*) FROM products UNION SELECT 'users' AS table_name, COUNT(*) UNION SELECT 'sales' AS table_name, COUNT(*)  FROM users", []);
+
+foreach ($countQuery as $value) {
+    $metrics[$value["table_name"]] = $value["amount"];
+}
+
 ?>
 
-<header>
-  <div class="user-infos">
-    <div class="user">
-      <i class="fas fa-user icon user-icon"></i>
-      <?php echo $username ?>
-    </div>
-    <form action="pages/logout.php" method="post">
-      <input type="submit" value="Desconectar-se" />
-    </form>
-  </div>
-</header>
-
-
 <main>
-  <header id="categories-header">
-    <h3>Administração de categorias</h2>
-    <button>Novo Registro</button>
-  </header>
-  <hr />
+    <div class="metrics">
+        <div class="box">
+            <h1>
+                <?php echo $metrics["categories"] ?>
+            </h1>
+            <p>Total de categorias</p>
 
-  <div>
-    <table class="table table-bordered table-striped table-hover">
-    <caption>Categorias disponíveis</caption>
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Status</th>
-          <th scope="col">Produtos na categoria</th>
-          <th scope="col-2">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-            $categoriesQuery = $databaseClass->query("SELECT * FROM `categories`");
-            foreach ($categoriesQuery as $key => $categoriesInfos) {
-                $categoryId = $categoriesInfos["id"];
-                $productsInCategory = $databaseClass->query("SELECT COUNT(id) AS `amount` FROM `products` WHERE `category_id` = '{$categoryId}'");
-                $isActive = $categoriesInfos['is_active'] == 1 ? "Ativada" : "Desativada";
+            <div class="box-icon">
+                <i class="fas fa-file icon"></i>
+            </div>
+        </div>
 
-                echo "<tr>";
-                    echo "<th scope='row'>{$categoryId}</th>";
-                    echo "<td>{$categoriesInfos['name']}</td>";
-                    echo "<td>{$isActive}</td>";
-                    echo "<td>";
-                        echo "<a href='#'>{$productsInCategory[0]['amount']}</a>";
-                    echo "</td>";
-                    echo "<td>";
-                        echo "<div class='buttons'>";
-                            echo "<button class='edit-button'>";
-                                echo "<i class='fas fa-pen icon'></i>";
-                            echo "</button>";
-                            echo "<button class='delete-button'>";
-                                echo "<i class='fas fa-trash icon'></i>";
-                            echo "</button>";
-                        echo "</div>";
-                    echo "</td>";
-                echo "</tr>";
-            }
-        ?>
-      </tbody>
-    </table>
-  </div>
+        <div class="box">
+            <h1>
+                <?php echo $metrics["products"] ?>
+            </h1>
+            <p>Total de produtos</p>
+
+            <div class="box-icon">
+                <i class="fas fa-cart-shopping icon"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <h1>
+                <?php echo $metrics["sales"] ?>
+            </h1>
+            <p>Total de vendas</p>
+
+            <div class="box-icon">
+                <i class="fas fa-receipt icon"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <h1>
+                <?php echo $metrics["users"] ?>
+            </h1>
+            <p>Total de usuários</p>
+
+            <div class="box-icon">
+                <i class="fas fa-users icon"></i>
+            </div>
+        </div>
+    </div>
 </main>
